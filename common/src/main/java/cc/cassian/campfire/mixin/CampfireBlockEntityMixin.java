07dab45c4dfb7b439e851cac80d1029d6315b5b8
Cassian Godsted <cassian@godsted.com>
2025-01-05 22:09:54 -0500

    1.21.4 port

--- common/src/main/java/cc/cassian/campfire/mixin/CampfireBlockEntityMixin.java
+++ common/src/main/java/cc/cassian/campfire/mixin/CampfireBlockEntityMixin.java
@@ -2,6 +2,10 @@ package cc.cassian.campfire.mixin;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.CampfireBlockEntity;
+import net.minecraft.recipe.CampfireCookingRecipe;
+import net.minecraft.recipe.ServerRecipeManager;
+import net.minecraft.recipe.input.SingleStackRecipeInput;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,7 +18,7 @@ import static cc.cassian.campfire.CampfireMod.applyPlayerEffects;
 @Mixin(CampfireBlockEntity.class)
 public class CampfireBlockEntityMixin {
     @Inject(method = "litServerTick", at = @At(value = "HEAD"))
-    private static void mixin(World world, BlockPos pos, BlockState state, CampfireBlockEntity campfire, CallbackInfo ci) {
+    private static void mixin(ServerWorld world, BlockPos pos, BlockState state, CampfireBlockEntity blockEntity, ServerRecipeManager.MatchGetter<SingleStackRecipeInput, CampfireCookingRecipe> recipeMatchGetter, CallbackInfo ci) {
         applyPlayerEffects(world, pos);
     }
 
