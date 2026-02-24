31a353942db5479753ecd701dc85485df0fd1748
Cassian <cassian@godsted.com>
2026-02-23 22:55:27 -0500

    stoves

--- src/main/java/cc/cassian/campfire/mixin/StoveBlockEntityMixin.java
+++ src/main/java/cc/cassian/campfire/mixin/StoveBlockEntityMixin.java
@@ -1,33 +1,32 @@
 package cc.cassian.campfire.mixin;
 
 import net.minecraft.core.BlockPos;
-//? if >1.21.2 {
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.crafting.CampfireCookingRecipe;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.SingleRecipeInput;
-//?} else {
 import net.minecraft.world.level.Level;
-//?}
 import net.minecraft.world.level.block.entity.CampfireBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import vectorwing.farmersdelight.common.block.StoveBlock;
+import vectorwing.farmersdelight.common.block.entity.StoveBlockEntity;
 
 import static cc.cassian.campfire.ComfortableCampfires.applyPlayerEffects;
 
-@Mixin(CampfireBlockEntity.class)
-public class CampfireBlockEntityMixin {
-    @Inject(method = "cookTick", at = @At(value = "HEAD"))
-    //? if >1.21.2 {
-    private static void mixin(ServerLevel level, BlockPos pos, BlockState blockState, CampfireBlockEntity campfireBlockEntity, RecipeManager.CachedCheck<SingleRecipeInput, CampfireCookingRecipe> cachedCheck, CallbackInfo ci)
-    //?} else {
-    /*private static void mixin(Level level, BlockPos pos, BlockState blockState, CampfireBlockEntity campfire, CallbackInfo ci)
-     *///?}
+@Pseudo
+@Mixin(StoveBlockEntity.class)
+public class StoveBlockEntityMixin {
+    @Inject(method = "cookingTick", at = @At(value = "HEAD"))
+    private static void mixin(Level level, BlockPos pos, BlockState blockState, StoveBlockEntity stove, CallbackInfo ci)
     {
-        applyPlayerEffects(level, pos, blockState);
+        boolean isStoveLit = blockState.getValue(StoveBlock.LIT);
+        if (isStoveLit)
+            applyPlayerEffects(level, pos, blockState);
     }
 
 }
